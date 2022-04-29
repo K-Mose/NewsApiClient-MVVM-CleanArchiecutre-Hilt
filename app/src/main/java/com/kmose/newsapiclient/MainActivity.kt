ff802ba6bd8c3551f72b41689dd5c321afa5097e
user <km_413@naver.com>
2022-04-29 15:42:27 +0900

    RecyclerView With DiffUtil

--- app/src/main/java/com/kmose/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/kmose/newsapiclient/MainActivity.kt
@@ -2,11 +2,20 @@ package com.kmose.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.kmose.newsapiclient.databinding.ActivityMainBinding
+import com.kmose.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.kmose.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +28,6 @@ class MainActivity : AppCompatActivity() {
         binding.btmNavNews.setupWithNavController(
             navController
         )
+        viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/kmose/newsapiclient/presentation/adapter/NewsAdapter.kt
@@ -0,0 +1,52 @@
+package com.kmose.newsapiclient.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kmose.newsapiclient.data.model.Article
+import com.kmose.newsapiclient.databinding.FragmentNewsBinding
+import com.kmose.newsapiclient.databinding.NewsListItemBinding
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+    // List의 Item이 다르면 교체해주는 Util class
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        var binding =  NewsListItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return NewsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.bind(article)
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    inner class NewsViewHolder(val binding: NewsListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            binding.tvTitle.text = article.title
+            binding.tvDescription.text = article.description
+            binding.tvPublishedAt.text = article.description
+            binding.tvSource.text = article.publishedAt
+            Glide.with(binding.ivArticleImage.context)
+                .load(article.urlToImage)
+                .into(binding.ivArticleImage)
+        }
+    }
+}
